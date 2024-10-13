9aa5fcd7028985efb6d1b156316394e882dc11e0
pdlpnk <rimchik3010@gmail.com>
2024-10-13 15:21:13 +0300

    Initial Commit

---
+++ PaletteExampleTests/PaletteExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteExampleTests.swift
+//  PaletteExampleTests
+//
+//  Created by Рома Подлипняк on 13.10.2024.
+//
+
+import Testing
+@testable import PaletteExample
+
+struct PaletteExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
